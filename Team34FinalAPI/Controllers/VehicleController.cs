148662bc562c801863f7dcb75376e74266087bc7
Busi Mpofu <u22492161@tuks.co.za>
2024-06-09 18:09:28 +0200

    This is the VehicleController

---
+++ Team34FinalAPI/Controllers/VehicleController.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Team34FinalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+    }
+}
