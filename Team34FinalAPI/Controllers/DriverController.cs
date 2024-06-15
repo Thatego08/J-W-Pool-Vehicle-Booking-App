9b4afd2a059ca5617b17c5b2c97288d75be68107
Thatego Malepe <thategomalepe08@gmail.com>
2024-06-15 23:25:39 +0200

    I added the UserDBContext

--- Team34FinalAPI/Controllers/DriverController.cs
+++ Team34FinalAPI/Controllers/DriverController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Reflection.Metadata.Ecma335;
+using Team34FinalAPI.Models;
 
 namespace Team34FinalAPI.Controllers
 {
@@ -7,5 +11,27 @@ namespace Team34FinalAPI.Controllers
     [ApiController]
     public class DriverController : ControllerBase
     {
+        private readonly IDriverRepository _driverRepository;
+
+        public DriverController(IDriverRepository driverRepository)
+        {
+            _driverRepository = driverRepository;
+        }
+
+        [HttpGet]
+        [Route("GetAllDrivers")] //Displays All The Drivers Stored On The Database
+
+        public async Task<IActionResult> GetAllDrivers()
+        {
+            try
+            {
+                var results = await _driverRepository.GetAllDriverAsync();
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "Internal Server Error. Please Contact Support");
+            }
+        }
     }
 }
---
+++ Team34FinalAPI/Models/UserDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Team34FinalAPI.Models
+{
+    public class UserDbContext : DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) 
+        { }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+
+            modelBuilder.Entity<User>().ToTable("Users");
+
+            //Seed data
+
+            modelBuilder.Entity<User>().HasData(
+
+                  new Driver
+                  {
+                     UserName = "NtsaMa",
+                     Name = "Ntsako",
+                     Surname = "Malepfana",
+                     Email = "NtsakoMalepfan@gmail.com",
+                     PhoneNumber = 0715341011,
+                     Password = "0000"
+
+                  },
+
+                    new Driver
+                    {
+                        UserName = "MahlSa",
+                        Name = "Sabelo",
+                        Surname = "Mahlangu",
+                        Email = "mahlanguSabelo@gmail.com",
+                        PhoneNumber = 0713341011,
+                        Password = "0000"
+
+                    }
+                );
+
+          
+        }
+    } 
+}
