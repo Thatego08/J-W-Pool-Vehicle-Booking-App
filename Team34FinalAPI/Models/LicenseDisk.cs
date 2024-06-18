9aef823135649bca496a31da0d5b86e38ee91858
Busi Mpofu <u22492161@tuks.co.za>
2024-06-18 18:26:37 +0200

    Hi Team. This is the LicenseModel added. I hope it makes sense.

---
+++ Team34FinalAPI/Models/LicenseDisk.cs
@@ -0,0 +1,20 @@
+﻿namespace Team34FinalAPI.Models
+{
+    public class LicenseDisk
+    {
+        public int Id { get; set; }
+
+        public int VehicleID { get; set; }
+
+        public DateTime LicenseExpiryDate { get; set; }
+
+        public string Status
+        {
+            get
+            {
+                return LicenseExpiryDate < DateTime.Today ? "Expired" : "Valid";
+            }
+        }
+    }
+
+}
