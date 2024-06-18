25b199666c0ba23354f173fd572c8e870c8b3977
Busi Mpofu <u22492161@tuks.co.za>
2024-06-18 18:30:27 +0200

    Hi Team. I added the Service Model.

---
+++ Team34FinalAPI/Models/Service.cs
@@ -0,0 +1,14 @@
+﻿namespace Team34FinalAPI.Models
+{
+    public class Service
+    {
+        public int ServiceID { get; set; }
+
+        public int VehicleID { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime ServiceDate { get; set; }
+
+    }
+}
