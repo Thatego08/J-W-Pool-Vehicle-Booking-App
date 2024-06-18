87f43b5ccf43af339026ee690cf7a3313d5e6cda
Busi Mpofu <u22492161@tuks.co.za>
2024-06-18 18:16:29 +0200

    Hi Team. This is the InusranceModel. Please check that it makes sense.

---
+++ Team34FinalAPI/Models/InsuranceCover.cs
@@ -0,0 +1,8 @@
+﻿namespace Team34FinalAPI.Models
+{
+    public class InsuranceCover
+    {
+        public int InsuranceCoverId { get; set; }
+        public string InsuranceCoverName { get; set; }
+    }
+}
