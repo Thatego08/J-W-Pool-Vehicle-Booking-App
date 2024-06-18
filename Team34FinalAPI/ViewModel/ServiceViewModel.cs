76c95c070452cf8bbe2cd49639178ea05d9f8853
Busi Mpofu <u22492161@tuks.co.za>
2024-06-18 14:33:13 +0200

    Hi team. I added the ViewModels ( VehicleViewModels and ServiceViewModel ).

---
+++ Team34FinalAPI/ViewModel/ServiceViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Team34FinalAPI.ViewModel
+{
+    public class ServiceViewModel
+    {
+        public int VehicleServiceID { get; set; }
+
+        public int VehicleID { get; set; }
+
+        public DateTime ServiceDate { get; set; }
+    }
+}
