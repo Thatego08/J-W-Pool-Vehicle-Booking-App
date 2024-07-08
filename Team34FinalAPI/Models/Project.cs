9c3c97e66e0df1df3254ac84fb00267cdab665db
welnah <85287109+welnah@users.noreply.github.com>
2024-07-08 18:57:39 +0200

    added the Rate controller and changed the project model

--- Team34FinalAPI/Models/Project.cs
+++ Team34FinalAPI/Models/Project.cs
@@ -3,15 +3,13 @@
     public class Project
     {
         public int ProjectID { get; set; }
-        public string ProjectName { get; set; }
+        public string ProjectNumber { get; set; }
         public int JobNo { get; set; }
         public int TaskCode { get; set; }
         public int ActivityCode { get; set; }
 
-
-        public decimal HalfDayRate { get; set; } 
-        public decimal FullDayRate { get; set; }
         // This line establishes the one-to-many relationship
         public ICollection<Booking> Bookings { get; set; }
+        public ICollection<Rate> Rates { get; set; }
     }
 }
